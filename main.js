7bfeccb67d04a1b12a894fcad049b8a788bb0cd8
Nick Nauert <nicknauert@gmail.com>
2017-10-20 13:54:40 -0400

    finalized

---
+++ main.js
@@ -0,0 +1,23 @@
+  document.querySelector("#aboutLink").addEventListener("click", function(e){
+    document.querySelector('.section-1').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+  })
+
+  document.querySelector("#sponsorsLink").addEventListener("click", function(e){
+    document.querySelector('.section-2').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+  })
+
+  document.querySelector("#eventLink").addEventListener("click", function(e){
+    document.querySelector('.video').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+  })
+
+  document.querySelector("#digitalForumLink").addEventListener("click", function(e){
+    document.querySelector('.video-section-info').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+  })
